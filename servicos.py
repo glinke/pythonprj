6c2dc9754d2bf01a82b67c25db689ae98035120d
root <EMAIL>
2026-09-04 17:09:35 -0300

    Adicionando os arquivos calculo.py servicos.py

---
+++ servicos.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+autor="Fabio Pires"
+portas_alvo = [22, 80, 443, 3306, 8080, 3000, 5000, 8000, 137, 138, 139, 445]
+portas_alvo.append(21)
+print('portas alvo: ', portas_alvo)
+print(portas_alvo[2])
+print(portas_alvo[-1])
+
+print('exemplo: ')
+for NUM in range(10):
+	print(NUM)
